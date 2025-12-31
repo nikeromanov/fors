322f1ec156b95818c9578f60b713ef52c2b679cb
nikeromanov <63921868+nikeromanov@users.noreply.github.com>
2025-12-31 12:46:00 +0300

    Add Yandex Metrica goal for general form

--- local/templates/fors/assets/js/script.js
+++ local/templates/fors/assets/js/script.js
@@ -70,13 +70,13 @@ $(document).ready(function(){
 							}
 						});
 					}
-					$.ajax({
-						type: "POST",
-						url: "/local/ajax/actions.php",
-						data: dataf,
-						processData: false,
-						contentType: false,
-					}).done(function( answ ){
+                                        $.ajax({
+                                                type: "POST",
+                                                url: "/local/ajax/actions.php",
+                                                data: dataf,
+                                                processData: false,
+                                                contentType: false,
+                                        }).done(function( answ ){
 						var data = JSON.parse(answ);
                                                 if(data.result=="success"){
                                                         var formContainer = $(form).closest('.consult-form');
@@ -145,11 +145,25 @@ $(document).ready(function(){
                                                                 $(form).find('.answer_form').append(data.message);
                                                         }
 
-                                                        $(form).find('input[type="text"]').val('');
-                                                        $(form).find('input[type="tel"]').val('');
-                                                        $(form).find('input[type="email"]').val('');
-                                                        $(form).find('input[type="checkbox"]').prop('checked', false);
-                                                        $(form).find('textarea').val('');
+                                                                $(form).find('input[type="text"]').val('');
+                                                                $(form).find('input[type="tel"]').val('');
+                                                                $(form).find('input[type="email"]').val('');
+                                                                $(form).find('input[type="checkbox"]').prop('checked', false);
+                                                                $(form).find('textarea').val('');
+
+                                                                if(typeof ym === 'function'){
+                                                                        if($(form).hasClass('consult-form__form')){
+                                                                                ym(11787892, 'reachGoal', 'form_general');
+                                                                        }
+
+                                                                        if($(form).hasClass('cars-gallery__contact-form')){
+                                                                                ym(11787892, 'reachGoal', 'form_car');
+                                                                        }
+
+                                                                        if(window.location && (window.location.pathname === '/online-traning/' || window.location.pathname === '/online-traning')){
+                                                                                ym(11787892, 'reachGoal', 'online_ok');
+                                                                        }
+                                                                }
 
 
                                                 }else{
