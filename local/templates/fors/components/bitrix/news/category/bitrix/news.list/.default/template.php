3331b0e226b391f8b198ccaf1cd0e7ce3e1973f8
Nike <qaqa@MacBook-Pro--IsWin.local>
2026-06-08 18:50:52 +0300

    Add category AggregateOffer description

--- local/templates/fors/components/bitrix/news/category/bitrix/news.list/.default/template.php
+++ local/templates/fors/components/bitrix/news/category/bitrix/news.list/.default/template.php
@@ -44,6 +44,10 @@ $this->setFrameMode(true);
 	if($schemaImageSrc !== "" && strpos($schemaImageSrc, "http://") !== 0 && strpos($schemaImageSrc, "https://") !== 0 && $schemaHost !== ""){
 		$schemaImageSrc = $schemaScheme . "://" . $schemaHost . $schemaImageSrc;
 	}
+	$schemaDescription = trim((string)html_entity_decode(strip_tags(htmlspecialcharsback((string)$section["DESCRIPTION"])), ENT_QUOTES | ENT_HTML5, 'UTF-8'));
+	if($schemaDescription === ""){
+		$schemaDescription = "Стоимость обучения по направлению «" . $categoryTitle . "» в автошколе «Форсаж».";
+	}
 	?>
 	<h1 class="category__title page-section__title" id="category-title" itemprop="name">
 	  <?=htmlspecialcharsbx($categoryTitle);?>
@@ -56,6 +60,7 @@ $this->setFrameMode(true);
 		  <?if($schemaOffers["HIGH_PRICE"] !== ""){?><meta itemprop="highPrice" content="<?=htmlspecialcharsbx((string)$schemaOffers["HIGH_PRICE"]);?>"><?}?>
 		  <?if($schemaOffers["LOW_PRICE"] !== ""){?><meta itemprop="lowPrice" content="<?=htmlspecialcharsbx((string)$schemaOffers["LOW_PRICE"]);?>"><?}?>
 		  <meta itemprop="priceCurrency" content="RUB">
+		  <meta itemprop="description" content="<?=htmlspecialcharsbx($schemaDescription);?>">
 		</div>
 	<?}?>
 	<div class="category__container">
